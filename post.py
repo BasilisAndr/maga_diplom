d8fefce59f953a4a058e2318bcf917a61f044416
BasilisAndr <blindedbysunshine@gmail.com>
2019-05-10 14:57:20 +0300

    postprocessing

---
+++ post.py
@@ -0,0 +1,27 @@
+import fileinput
+from pprint import pprint
+
+res_all = []
+res = {}
+current = ''
+for line in fileinput.input():
+    line = line.strip()
+    if line:
+        pieces = line.split('\t')
+        current = pieces[-3]
+        if float(pieces[-1]) in res:
+            res[float(pieces[-1])].append(pieces[-2])
+        else:
+            res[float(pieces[-1])] = [pieces[-2]]
+    else:
+        res_all.append([current, res])
+        res = {}
+
+
+for word, item in res_all:
+    if len(item) == 1 and item[list(item.keys())[0]][0].endswith('?'):
+        print(f'^{word}/*{word}')
+    else:
+        maxx = max(item.keys())
+        raz = '/'.join(item[maxx])
+        print(f'^{word}/{raz}')
